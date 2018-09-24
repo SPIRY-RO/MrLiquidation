2c62d2461d3225c2029c6d979d1009b15c400af4
SpiryɃTC <spirymedia@gmail.com>
2018-09-24 22:07:48 +0300

    x

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,4 +1,4 @@
-const HELP_TEXT = "<b>Eu sunt Domnul Lichidare.\n" +
+const HELP_TEXT = "<b>Eu sunt Domnul Lichidare.</b>\n" +
   "<b>Pentru preturi live:</b>\n"; +
   "/price [BASE] [QUOTE]\n" +
   "/providers - see the list of supported exchanges\n" +
