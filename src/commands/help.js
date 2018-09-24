478004625f4c24145defeb8e08826ef4b60471b0
SpiryɃTC <spirymedia@gmail.com>
2018-09-24 22:00:10 +0300

    Mr Liquidation

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,9 +1,10 @@
-const HELP_TEXT = "<b>Blockchain Info Bot - by libertylocked</b>\n" +
-  "/price - see last trade prices on multiple exchanges. Usage: /price [BASE] [QUOTE]\n" +
+const HELP_TEXT = "<b>Eu sunt Domnul Lichidare si acestea sunt comenzile mele.\n" +
+  "<b>Pentru preturi live:</b>\n"; +
+  "/price [BASE] [QUOTE]\n" +
   "/providers - see the list of supported exchanges\n" +
   "/eth - ethereum web3 functions (advanced users only). Geth node provided by INFURA\n" +
-  "/help - see the list of commands\n" +
-  "\n<b>GitHub</b>: <a>https://github.com/libertylocked/telegram-cryptotrader</a>";
+  "/help - see the list of commands\n";
+ 
 
 const handler = (msg, reply) => {
   reply.html(HELP_TEXT);
