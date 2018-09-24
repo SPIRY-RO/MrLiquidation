0efd0b16d86eccb75f311d88eafee05abd6a59a3
SpiryɃTC <spirymedia@gmail.com>
2018-09-25 00:26:07 +0300

    Telegraf vs Botgram

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const Botgram = require("botgram");
 const config = require("./config");
+const Telegraf = require('telegraf')
+
 if (!config.botToken) {
   console.error("No bot token specified. Please set BOT_TOKEN in env");
   process.exit(1);
 }
 
-const bot = new Botgram(config.botToken);
+const bot = new Telegraf(config.botToken);
 
-bot.command("pretul", require("./commands/price"));
-bot.command("exchanges", require("./commands/providers"));
-bot.command("eth", require("./commands/eth"));
-bot.command("help", "start", require("./commands/help"));
-bot.command("ceasul", function ( reply ) {
-  reply.text("Orologiu arata: " + Date());
-});
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
