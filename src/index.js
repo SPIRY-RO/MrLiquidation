9d6ed235f8cb32ced848db81b0bc9ce8b0a3ef58
SpiryɃTC <spirymedia@gmail.com>
2018-09-25 00:03:22 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const Botgram = require("botgram");
 const config = require("./config");
-
+const Telegraf = require('telegraf')
 if (!config.botToken) {
   console.error("No bot token specified. Please set BOT_TOKEN in env");
   process.exit(1);
 }
 
-const bot = new Botgram(config.botToken);
-
+const bot = new Telegraf(config.botToken);
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
 bot.command("pretul", require("./commands/price"));
-bot.command("mcap", require("./commands/mcap"));
 bot.command("exchanges", require("./commands/providers"));
 bot.command("eth", require("./commands/eth"));
 bot.command("help", "start", require("./commands/help"));
+bot.startPolling()
