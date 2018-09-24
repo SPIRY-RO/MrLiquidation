5558b7db027cee567387cc53902bffad0f22a982
SpiryɃTC <spirymedia@gmail.com>
2018-09-24 22:44:32 +0300

    Update

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,8 +4,11 @@ const HELP_TEXT = '<b>Eu sunt Domnul 🎩 Lichidare</b>\n' +
   "\n" +
   "<b>Pentru preturi live:</b>\n" +
   "/price [BASE] [QUOTE]\n" +
+  "<b>Lista de exchange-uri care le support:</b>\n" +
   "/providers - see the list of supported exchanges\n" +
-  "/eth - ethereum web3 functions (advanced users only). Geth node provided by INFURA\n" +
+  "<b>Daca esti baiat bun, iti pot verifica si balanta pe Ehtereum.</b>\n" +
+  "/eth \n" +
+  "<b>Mai multe comenzii soon. Eu Domnul🎩 Lichidare va stau la dispozitie! </b>\n" +
   "/help - see the list of commands\n";
  
 
