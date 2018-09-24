1e50270b6090bd3a08914e26bc7badf765be8cc3
SpiryɃTC <spirymedia@gmail.com>
2018-09-24 23:29:30 +0300

    test

---
+++ src/keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+    TELEGRAM: {
+      TOKEN: 'TELEGRAM_BOT_TOKEN'
+    },
+    COINMARKETCAP: {
+      API_KEY: 'CMC_PRO_API_KEY'
+    },
+    COINMARKETCAL: {
+      CLIENT_ID: 'CLIENT_ID',
+      CLIENT_SECRET: 'CLIENT_SECRET'
+    }
+  }
+  
